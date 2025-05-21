8095b7c78a7d3ed29395fec8893ee8978703d6a6
snowinmars <snowinmars@yandex.ru>
2025-05-21 08:00:53 +0400

    Add Morte sprites

---
+++ src/game/dlg/transforms.py
@@ -0,0 +1,6 @@
+import renpy
+
+center_left = renpy.display.transform.Transform(xalign=0.4, yalign=0.5)
+center_right = renpy.display.transform.Transform(xalign=0.6, yalign=0.5)
+center_left_down = renpy.display.transform.Transform(xalign=0.4, yalign=0.4)
+center_right_down = renpy.display.transform.Transform(xalign=0.6, yalign=0.4)
