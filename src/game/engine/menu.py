e1a722260e8d2825147f07a261655116c17123c9
snowinmars <snowinmars@yandex.ru>
2025-05-26 20:23:28 +0400

    Extract menu building

---
+++ src/game/engine/menu.py
@@ -0,0 +1,75 @@
+class MenuOption:
+    def __init__(self, title, label_id, condition, priority=0):
+        self.title = title
+        self.label_id = label_id
+        self.condition = condition
+        self.priority = priority
+
+class MenuBuilder:
+    def __init__(self, menu_name):
+        self._menu_name = menu_name
+        self._options = []
+        self._current_option = None
+
+    def option(self, title):
+        self._complete()
+        """Begin defining a new menu option"""
+        self._current_option = {
+            'title': title,
+            'label_id': None,
+            'condition': None,
+            'priority': 0
+        }
+        return self
+
+    def jump(self, target_title):
+        """Set the jump target for the current option"""
+        self._current_option['label_id'] = target_title
+        return self
+
+    def when(self, condition):
+        """Set the condition for the current option"""
+        self._current_option['condition'] = condition
+        return self
+
+    def priority(self, value):
+        """Set the priority for the current option"""
+        self._current_option['priority'] = value
+        return self
+    def done(self, manager):
+        """Register all built options with a MenuManager"""
+        if not self._menu_name:
+            raise Exception("Menu name not specified")
+
+        self._complete()
+        for option in self._options:
+            manager.add_option(self._menu_name, option)
+        return manager
+
+    def _complete(self):
+        if self._current_option is None:
+            return
+        """Complete and add the current option to the menu"""
+        if not self._current_option['title'] or not self._current_option['label_id']:
+            raise Exception("Incomplete menu option")
+        self._options.append(MenuOption(
+            title=self._current_option['title'],
+            label_id=self._current_option['label_id'],
+            condition=self._current_option['condition'],
+            priority=self._current_option['priority']
+        ))
+
+class MenuManager:
+    def __init__(self):
+        self.menu_options = {}
+
+    def add_option(self, menu_name, option):
+        if menu_name not in self.menu_options:
+            self.menu_options[menu_name] = []
+        self.menu_options[menu_name].append(option)
+        self.menu_options[menu_name].sort(key=lambda x: x.priority, reverse=True) # set highest first
+
+    def get_available_options(self, menu_name):
+        if menu_name not in self.menu_options:
+            return []
+        return [opt for opt in self.menu_options[menu_name] if opt.condition is not None and opt.condition()]
---
+++ src/game/labels/morgue_menu.py
@@ -0,0 +1,73 @@
+import renpy
+from settings.settings_global import (current_global_settings)
+from settings.settings_morgue import (current_morgue_settings)
+from engine.menu import (MenuBuilder)
+
+def build_morgue_menu(menu_manager):
+    MenuBuilder("morgue_main") \
+        .option("Поговорить с Мортом") \
+        .jump("dmorte_one_talk_morte") \
+        .when(lambda: current_global_settings()['in_party_morte'] \
+            and current_global_settings()['location'] == 'morgue1' \
+            and not current_global_settings()['dead_morte']) \
+        \
+        .option("Поговорить с Мортом") \
+        .jump("dmorte_two_talk_morte") \
+        .when(lambda: current_global_settings()['in_party_morte'] \
+            and current_global_settings()['location'] == 'morgue2' \
+            and not current_global_settings()['dead_morte']) \
+        \
+        .option("Убить Морта") \
+        .jump("dmorte_one_kill_morte") \
+        .when(lambda: current_global_settings()['meet_morte'] \
+            and current_global_settings()['in_party_morte'] \
+            and not current_global_settings()['dead_morte']) \
+        \
+        .option("Пригласить Морта в группу") \
+        .jump("dmorte_one_join") \
+        .when(lambda: not current_global_settings()['in_party_morte']) \
+        \
+        .option("Атаковать плешивый ходячий труп") \
+        .jump("dmorte_one_kill_dzm569") \
+        .when(lambda: not current_morgue_settings()['dead_dzm569']) \
+        \
+        .option("Атаковать ходячий труп повешенного") \
+        .jump("dmorte_one_kill_dzm825") \
+        .when(lambda: not current_morgue_settings()['dead_dzm825']) \
+        \
+        .option("Атаковать ходячий труп, полный ненависти") \
+        .jump("dmorte_one_kill_dzm782") \
+        .when(lambda: not current_morgue_settings()['dead_dzm782']) \
+        \
+        .option("Поговорить с ходячим и плешивым трупом") \
+        .jump("dmorte_one_talk_dzm569") \
+        .when(lambda: not current_morgue_settings()['dead_dzm569']) \
+        \
+        .option("Поговорить с ходячим трупом повешенного") \
+        .jump("dmorte_one_talk_dzm825") \
+        .when(lambda: not current_morgue_settings()['dead_dzm825']) \
+        \
+        .option("Поговорить с ходячим трупом, полным ненависти") \
+        .jump("dmorte_one_talk_dzm782") \
+        .when(lambda: not current_morgue_settings()['dead_dzm782']) \
+        \
+        .option("Попробовать открыть одну из дверей") \
+        .jump("dmorte_one_open_morgue_door") \
+        .when(lambda: current_morgue_settings()['has_intro_key']) \
+        \
+        .option("Подойти к существу около большой книги") \
+        .jump("dmorte_two_meet_dhall") \
+        .when(lambda: current_morgue_settings()['saw_dhall'] \
+            and not current_global_settings()['meet_dhall']) \
+        \
+        .option("Поговорить с Дхаллом") \
+        .jump("dmorte_two_talk_dhall") \
+        .when(lambda: current_morgue_settings()['saw_dhall'] \
+        and current_global_settings()['meet_dhall'] \
+            and not current_global_settings()['dead_dhall']) \
+        \
+        .option("Убить Дхалла") \
+        .jump("dmorte_two_kill_dhall") \
+        .when(lambda: current_global_settings()['meet_dhall'] \
+            and not current_global_settings()['dead_dhall']) \
+        .done(menu_manager)
